b004f4cfc06dbc075f1ac9aab570c8905adc1c6f
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2012-10-31 14:55:37 -0200

    Typo

--- application/views/wip.php
+++ application/views/wip.php
@@ -2,7 +2,7 @@
     <h2>Work In Progress</h2>
     
     <p>We have recently released a new version of the web site, and now we are 
-    working on video casts, tutorials and more plug-ins. Would you like to help 
+    working on videocasts, tutorials and more plug-ins. Would you like to help 
     us? <a href='{base_url}contact' title='Contact'>Get in touch</a></p>
     
     <p><img src='{base_url}assets/img/scientist.gif' title='Scientist' /></p>
